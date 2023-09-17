e0b9031e935fbb1d45d8e5064bf7790f1581be80
nikhilkumarjha62 <nikhilkumarjha610@gmail.com>
2023-09-17 17:38:25 +0530

    videohub

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React , { StrictMode} from 'react'
+import { ChakraProvider, ColorModeScript,theme } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import ColorModeSwitcher from './ColorModeSwitcher';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ColorModeScript />
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
